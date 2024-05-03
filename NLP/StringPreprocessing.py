5f88a028898452342cf90e10a29535f3b8c2ca88
Ahmed Hany <ah7608867@gmail.com>
2024-05-03 19:25:43 +0300

    KMP

--- NLP/StringPreprocessing.py
+++ NLP/StringPreprocessing.py
@@ -66,6 +66,29 @@ class Hash:
     def equal(self, l1, r1, l2, r2):
         return self.sub(l1, r1) == self.sub(l2, r2)
 
+class KMPSearch:
+    
+    def prefix_function(a):
+        n = len(a)
+        prefix = [0] * n
+        for i in range(1, n):
+            j = prefix[i - 1]
+            while j > 0 and a[i] != a[j]:
+                j = prefix[j - 1]
+            j += (a[i] == a[j])
+            prefix[i] = j
+        return prefix
+
+    def occurrences_of_each_prefix(prefix, n):
+        ret = [0] * (n + 1)
+        for i in range(n):
+            ret[prefix[i]] += 1
+        for i in range(n - 1, 0, -1):
+            ret[prefix[i - 1]] += ret[i]
+        for i in range(n + 1):
+            ret[i] += 1
+        return ret
+
 
 class text_processing(BaseEstimator, TransformerMixin):
     def __init__(
